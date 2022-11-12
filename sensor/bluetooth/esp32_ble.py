cea8b2476fd6a85dd5dd5c7b3532d665a495d95c
li.bowei <1306475495@qq.com>
2022-11-12 22:53:19 +0800

    feat: 蓝牙实现

--- sensor/bluetooth/esp32_ble.py
+++ sensor/bluetooth/esp32_ble.py
@@ -1,51 +1,38 @@
 from time import sleep_ms
 
 import bluetooth
-from machine import Pin, Timer
-import network
+from machine import Pin
 
 BLE_MSG = ""
 
 
-class ESP32_BLE:
-
+class ESP32_BLE():
     def __init__(self, name):
-        self.led = Pin(2, Pin.OUT)
-        self.timer = Timer(0)
+        self.led = Pin(14, Pin.OUT)
         self.name = name
         self.ble = bluetooth.BLE()
         self.ble.active(True)
         self.ble.config(gap_name=name)
-        self.disconnected()
         self.ble.irq(self.ble_irq)
         self.register()
         self.advertiser()
 
-    def connected(self):
-        self.led.off()
-        self.timer.deinit()
-
-    def disconnected(self):
-        self.timer.init(mode=Timer.PERIODIC, period=100, callback=lambda t: self.led.value(not self.led.value()))
-
     def ble_irq(self, event, data):
         global BLE_MSG
-        ## 时间手机链接了此设备
-        if event == 1:
-            self.connected()
-            ## 断开链接
-        elif event == 2:
+        if event == 1:  # _IRQ_CENTRAL_CONNECT 手机链接了此设备
+            self.led.value(0)
+        elif event == 2:  # _IRQ_CENTRAL_DISCONNECT 手机断开此设备
             self.advertiser()
-            self.disconnected()
-        elif event == 3:
+            self.led.value(1)
+        elif event == 3:  # _IRQ_GATTS_WRITE 手机发送了数据
             buffer = self.ble.gatts_read(self.rx)
-            BLE_MSG = buffer.decode("UTF-8").strip()
-        pass
+            BLE_MSG = buffer.decode('UTF-8').strip()
+
 
     def register(self):
-        service_uuid = "1a4361f1-0bbe-4c1c-a94a-7fdb2d7b4675"
-        reader_uuid = "1a4361f1-0bbe-4c1c-a94a-7fdb2d7b4675"
-        sender_uuid = "1a4361f1-0bbe-4c1c-a94a-7fdb2d7b4675"
+        service_uuid = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
+        reader_uuid = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
+        sender_uuid = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
 
         services = (
             (
@@ -58,44 +45,23 @@ class ESP32_BLE:
         )
         ((self.tx, self.rx,),) = self.ble.gatts_register_services(services)
 
-    def send(self, message):
-        self.ble.gatts_notify(0, self.tx, message + "\n")
+    def send(self, data):
+        self.ble.gatts_notify(0, self.tx, data + '\n')
 
     def advertiser(self):
-        name = bytes(self.name, "UTF-8")
-        adv_data = bytearray("\x02\x02\x02") + bytearray((len(name) + 1, 0x09)) + name
+        name = bytes(self.name, 'UTF-8')
+        adv_data = bytearray('\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name
         self.ble.gap_advertise(100, adv_data)
         print(adv_data)
         print("\r\n")
 
 
-def buttons_irq(pin):
-    pass
+if __name__ == "__main__":
+    ble = ESP32_BLE("ESP32BLE")
 
-def connect_wifi(name, password):
-    wlan = network.WLAN(network.STA_IF)
-    if wlan.isconnected():
-        # print(f"esp8266 is already connection wifi, config:{wlan.ifconfig()}")
-        return wlan
-    wlan.active(True)
-    print("connecting to wifi")
-    wlan.connect(name, password)
-    while not wlan.isconnected():
-        print("connecting...")
-        sleep_ms(100)
-    return wlan
-
-
-if __name__ == '__main__':
-    connect_wifi("gonewiththewind", "wangtao0303")
-    ble = ESP32_BLE("esp32-bluetooth")
-    but = Pin(0, Pin.IN)
-    but.irq(trigger=Pin.IRQ_FALLING, handler=buttons_irq)
-    led = Pin(2, Pin.OUT)
     while True:
-        if BLE_MSG == "read":
+        if BLE_MSG == 'read_LED':
             print(BLE_MSG)
+            ble.send('hello' + BLE_MSG)
             BLE_MSG = ""
-            print("led is on")
-            ble.send("led is on")
         sleep_ms(100)
